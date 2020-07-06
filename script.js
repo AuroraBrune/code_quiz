cc24cb9e48aeeb391a06029002faefb09f71a3fe
Aurora Brune <akbrune@hotmail.com>
2020-07-05 19:05:37 -0500

    Added changes to questions and buttons.

--- script.js
+++ script.js
@@ -1,129 +1,107 @@
 
-const quizContainer = document.getElementById('quiz');
-const resultsContainer = document.getElementById('results');
-const submitButton = document.getElementById('submit');
-var countdownTimer = document.getElementById("countdownTimer");
+//setting my questions array, could build using JSON object
+let myQuestions = [];
+const question1 = ["1. What does NaN mean in code?", "Not a name.", "Name and number.", "Not a number.", "3"];
+const question2 = ["2. Which HTML element do we use for JavaScript?", "<scripting>", "<js>", "script", "3"];
+const question3 = ["3. The statement a === b refers to ", "Both a and b are equal in value, type and reference address.", "Both a and b are equal in value.",
+    "Both a and b are equal in value and type.", "3"];
+const question4 = ["4. Which of the following best describes JavaScript?", "a compiled scripting language.",
+    " an object-oriented scripting language.",
+    " a scripting language precompiled in the browser.", "2"];
+const question5 = ["5. Which of the following is the structure of an if statement?", "if (conditional expression is true) thenexecute this codeend if",
+    "if (conditional expression is true)execute this codeend if",
+    "if (conditional expression is true)   {then execute this code>->}", "3"];
 
-var timeLeft = 75;
-var downloadTimer = setInterval(function () {
-    timeLeft--;
-   
-    countdownTimer.textContent = "You have " + timeLeft + " seconds to answer the question."
-    if (timeLeft <= 0) {
-        clearInterval(downloadTimer);
+myQuestions.push(question1);
+myQuestions.push(question2);
+myQuestions.push(question3);
+myQuestions.push(question4);
+myQuestions.push(question5);
+let currentIndex = 0;
+const countdownTimer = document.getElementById("countdownTimer");
+
+let secondsLeft = 75;
+const timer = setInterval(function () {
+    secondsLeft--;
+
+    countdownTimer.innerHTML = secondsLeft;
+    if (secondsLeft <= 0) {
+        clearInterval(timer);
     }
 }, 1000);
+//initiallizing first question on page
+showMyQuestions(); 
+
+function showMyQuestions() {
+    var question = myQuestions[currentIndex];
+
+    document.getElementById("question_text").innerHTML = question[0];
+    document.getElementById("btna").innerHTML = question[1];
+    document.getElementById("btnb").innerHTML = question[2];
+    document.getElementById("btnc").innerHTML = question[3];
+
+}
 
 
-//Objects to hold my questions
-const myQuestions = [
+function score() {
+alert("we have a score " )
+
+}
+
+//Object containing my questions
+const myQuestionsJson = [
     {
-        question: "What does NaN mean in code?",
-        answers: {
-            a: "Not a name.",
-            b: "Name and number.",
-            c: "Not a number."
 
-        },
-        correctAnswer: "d"
+        question: "1. What does NaN mean in code?",
+        choices: [
+            "Not a name.",
+            "Name and number.",
+            "Not a number."
+        ],
+        answer: "Not a number."
     },
 
     {
-        question: "Which HTML element do we use for JavaScript?",
-        answers: {
-            a: "<scripting>",
-            b: "<js>",
-            c: "java",
-            d: "script",
-        },
-        correctAnswer: "d"
+        question: "2. Which HTML element do we use for JavaScript?",
+        choices: [
+            "<scripting>",
+            "<js>",
+            "script"
+        ],
+        answer: "script"
     },
 
     {
-        question: "The statement a === b refers to ",
-        answers: {
-            a: "Both a and b are equal in value, type and reference address.",
-            b: "Both a and b are equal in value.",
-            c: "Both a and b are equal in value and type. ",
-            d: "None of the above. ",
-        },
-        correctAnswer: "c"
+        question: "3. The statement a === b refers to ",
+        choices: [
+            "Both a and b are equal in value, type and reference address.",
+            "Both a and b are equal in value.",
+            "Both a and b are equal in value and type."
+        ],
+        answer: "Both a and b are equal in value and type."
     },
 
     {
-        question: "  Which of the following best describes JavaScript?",
-        answers: {
-            a: "a compiled scripting language.",
-            b: " an object-oriented scripting language.",
-            c: " a scripting language precompiled in the browser.",
-            d: " a low-level programming language.",
-        },
-        correctAnswer: "b"
+        question: "4. Which of the following best describes JavaScript?",
+        choices: [
+            "a compiled scripting language.",
+            " an object-oriented scripting language.",
+            " a scripting language precompiled in the browser."
+        ],
+        answer: " an object-oriented scripting language."
     },
 
     {
-        question: "Which of the following is the structure of an if statement?",
-        answers: {
-            a: "if (conditional expression is true) thenexecute this codeend if",
-            b: "if (conditional expression is true)execute this codeend if",
-            c: "if (conditional expression is true)   {then execute this code>->}",
-            d: "if (conditional expression is true) then {execute this code}",
-        },
-        correctAnswer: "c"
+        question: "5. Which of the following is the structure of an if statement?",
+        choices: [
+            "if (conditional expression is true) thenexecute this codeend if",
+            "if (conditional expression is true)execute this codeend if",
+            "if (conditional expression is true)   {then execute this code>->}"
+        ],
+        answer: "if (conditional expression is true)   {then execute this code>->}"
     }
 ];
 
-function buildQuiz() {
-    //var to store output
-    const output = [];
-
-    myQuestions.forEach(
-        (currentQuestion, questionNumber) => {
-            const answers = [];
-
-            for (letter in currentQuestion.answers) {
-                answers.push(
-                   ` <label>
-                        <input type="radio" name="question${questionNumber}" value="${letter}">
-                            ${letter} :
-                        ${currentQuestion.answers[letter]}
-                    </label>`
-                )
-            }
-    
-            output.push(
-                `<div class="question"> ${currentQuestion.question} </div>
-                        <div class="answers"> ${answers.join('')} </div>`
-            )
-            console.log(answers)
-        }
-    )
-    quizContainer.innerHTML = output.join('');
 
 
-
-
-
-    myQuestions.forEach( (currentQuestion, questionNumber) =>  {
-        const answers = [];
-        for(letter in currentQuestion.answers) {
-            answers.push(
-                `<label>
-                <input type="radio" name="question${questionNumber}" value="${letter}" >
-                ${letter} :
-                ${currentQuestionl.answers[letter]}
-                </label>`
-
-            );
-        }
-
-        output.push(
-            `<div class="question"> ${currentQuestion.question} </div>`
-            `<div class="answers"> ${answers.join('') }</div>`
-        );
-    }
-    );
-}
-
-            // submitButton.addEventListener('click', showResults);
 
